3bd95fe6f340c14cd5fc068d45163e5265758397
Dylan Awalt-Conley <web@dylan.ac>
2024-09-11 14:06:58 -0400

    configure dev and non-dev dependencies
    Able to run npm run build after installing with NODE_ENV=production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
+const devModules =
+    process.env.NODE_ENV === 'production' ? [] : ['@nuxt/eslint'];
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['@nuxt/ui', '@nuxt/eslint'],
+    modules: ['@nuxt/ui', ...devModules],
     compatibilityDate: '2024-08-20',
     runtimeConfig: {
         siteName: 'SERP Best',
