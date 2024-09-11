bb487372b2532feed39079cf696ed00b918fe0c8
Dylan Awalt-Conley <web@dylan.ac>
2024-09-11 14:17:27 -0400

    include @nuxt/eslint plugin in production
    Needed because it's a nuxt module, and nuxt reads it from the config
    during build. Not a great system imo but I was unable to get it working
    as a dev dependency.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
-const devModules =
-    process.env.NODE_ENV === 'production' ? [] : ['@nuxt/eslint'];
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['@nuxt/ui', ...devModules],
+    modules: ['@nuxt/ui', '@nuxt/eslint'],
     compatibilityDate: '2024-08-20',
     runtimeConfig: {
         siteName: 'SERP Best',
