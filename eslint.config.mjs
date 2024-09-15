b6331258bc925b5813ecbe07b9170bec40717ae1
Dylan Awalt-Conley <web@dylan.ac>
2024-09-14 21:44:07 -0400

    enforce order of vue component blocks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
   rules: {
+    'vue/block-order': ['error', { order: ['template', 'script', 'style'] }],
     'vue/no-setup-props-destructure': 'error',
     '@typescript-eslint/no-unused-vars': 'warn',
     'no-console': 'warn',
