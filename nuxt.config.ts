6f2108791156c3763161610bcea454bbfc27f8fa
Dylan Awalt-Conley <web@dylan.ac>
2024-10-03 13:09:34 -0400

    use site variables on legal pages
    Legal pages will update based on the site config.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   site: {
-    // url: 'https://test.com',
+    url: 'https://best.serp.co',
     name: 'SERP Best',
     desription: 'Site description',
     defaultLocale: 'en',
     trailingSlash: true,
+    email: {
+      contact: 'support@serp.ai',
+      dmca: 'dmca@serp.ai',
+      ipcomplaints: 'ipcomplaints@serp.ai',
+    },
   },
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/content', '@nuxtjs/seo'],
