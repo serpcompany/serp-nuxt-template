892740913798c00d53e505ad63d2d468274ecfa2
Dylan Awalt-Conley <web@dylan.ac>
2024-10-04 22:33:07 -0400

    delete unused post slots

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -4,27 +4,11 @@
     <!--   <SiteBreadcrumbs :links="breadcrumbs" /> -->
     <!-- </template> -->
     <UPage v-show="post" class="mx-auto max-w-[1408px] px-4">
-      <template #left>
-        <!-- <UContentToc v-if="post.body?.toc?.links?.length" :links="post.body.toc.links" /> -->
-      </template>
-
       <UPageBody prose class="mx-auto max-w-prose">
         <h1>{{ post.title }}</h1>
         <!-- eslint-disable-next-line vue/no-v-html -- no other option at the moment: post content is trusted and cannot be user-authored -->
         <div v-html="post.content" />
       </UPageBody>
-
-      <template #right>
-        <UAside
-          class="bg-background/75 sticky top-[--header-height] -mx-4 max-h-[calc(100vh-var(--header-height))] overflow-y-auto px-4 backdrop-blur sm:-mx-6 sm:px-6 lg:-mx-4 lg:px-4"
-        >
-          <div
-            class="space-y-3 border-b border-dashed border-gray-200 lg:border-0 dark:border-gray-800"
-          >
-            <h2 class="mb-4 truncate text-sm/6 font-semibold" />
-          </div>
-        </UAside>
-      </template>
     </UPage>
   </NuxtLayout>
 </template>
